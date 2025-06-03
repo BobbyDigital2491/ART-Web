6211a1b84a38c8c78b5d892ac4624aa1269f928d
Robbob24 <r.lawrence2491@gmail.com>
2025-06-03 16:18:48 -0400

    12th

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -7,4 +7,4 @@ if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase URL or Anon Key");
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
